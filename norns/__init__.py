4be429ea7c75138a85caf001869d9b36d09d8fd5
siebrenf <siebrenf@gmail.com>
2020-03-25 21:42:21 +0100

    added `from norns.exceptions import ConfigError` to init, polished README

--- norns/__init__.py
+++ norns/__init__.py
@@ -1,4 +1,7 @@
 import norns.cfg
 
+from norns.exceptions import ConfigError
+
+
 def config(name=None, config_file=None, default=None):
     return norns.cfg.Config(name=name, config_file=config_file, default=default)
